cbe75fe9368c79c65e5f3f8ea9d4fbf10fe23a93
thv <24560077@gm.uit.edu.vn>
2026-02-13 15:27:38 +0700

    Create script.js

---
+++ Week4/Gallery/script.js
@@ -0,0 +1,8 @@
+function openModal(src) {
+    document.getElementById("myModal").style.display = "flex";
+    document.getElementById("img01").src = src;
+}
+
+function closeModal() {
+    document.getElementById("myModal").style.display = "none";
+}
